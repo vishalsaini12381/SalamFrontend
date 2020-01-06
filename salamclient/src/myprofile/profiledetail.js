fd254d011296c76f152a9d4cc00d63e782b03bde
Ravi Basil <ravi09basil@rediffmail.com>
2020-01-06 19:34:29 +0530

    constant issues

--- salamclient/src/myprofile/profiledetail.js
+++ salamclient/src/myprofile/profiledetail.js
@@ -165,16 +165,16 @@ class Profiledetail extends React.Component {
 
   maxDateForDOBInput = () => {
     let today = new Date();
-    const dd = today.getDate();
-    const mm = today.getMonth() + 1; //January is 0!
-    const yyyy = today.getFullYear();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+    let yyyy = today.getFullYear();
     if (dd < 10) {
       dd = '0' + dd
     }
     if (mm < 10) {
       mm = '0' + mm
     }
-
+s
     today = yyyy + '-' + mm + '-' + dd;
     return today
   }
